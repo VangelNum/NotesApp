ff1c75d5d965ab2651befe5125c6432dbb1f1e3e
VangelNum <vangelnum@gmail.com>
2022-11-01 21:35:32 +0400

    Initial commit

---
+++ app/src/main/java/com/vangelnum/notesapp/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.vangelnum.notesapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.vangelnum.notesapp.navigation.NotesNavHost
+import com.vangelnum.notesapp.ui.theme.NotesAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NotesAppTheme {
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text(text = "Notes App")
+                            },
+                            backgroundColor = Color.Blue,
+                            contentColor = Color.White,
+                            elevation = 12.dp
+                        )
+                    }
+                ) {
+                    NotesNavHost()
+                }
+            }
+        }
+    }
+}
+
---
+++ app/src/main/java/com/vangelnum/notesapp/screens/Note.kt
@@ -0,0 +1,10 @@
+package com.vangelnum.notesapp.screens
+
+import androidx.compose.material.Button
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun Note(navController: NavHostController) {
+
+}
